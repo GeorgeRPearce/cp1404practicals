e71ddbcb8163f0c70c0710efbc8d754da3ae92d7
George Pearce <37967836+GeorgeRPearce@users.noreply.github.com>
2024-10-28 04:29:27 +1000

    prac 3

---
+++ prac 3/randoms.py
@@ -0,0 +1,19 @@
+"""
+What did you see on line 1?
+6
+What was the smallest number you could have seen, what was the largest?
+5, 20
+What did you see on line 2?
+5
+What was the smallest number you could have seen, what was the largest?
+3, 9
+Could line 2 have produced a 4?
+no
+
+What did you see on line 3?
+4.315341799378929
+What was the smallest number you could have seen, what was the larges
+2.50000, 5.50000
+"""
+import random
+print(random.randint(1,100))
